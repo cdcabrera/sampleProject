c6ff9bab355e48868b9ebe5772bec0da8bfb3732
CD Cabrera <cabrera.code@gmail.com>
2017-02-21 02:10:40 -0500

    Clean Up
    Removed useless

--- src/services/users/filter.user-object.js
+++ src/services/users/filter.user-object.js
@@ -16,25 +16,14 @@
         .filter('userObjectFilter', [
             function() {
 
-                function descendingSort(a, b) {
-
-                    return b - a;
-                }
-
 
                 return function(userArr) {
 
                     var byId = {};
 
-                    userArr.sort(descendingSort);
-
                     angular.forEach(userArr, function(userObj, index) {
 
                         byId[userObj.id] = userObj;
-                        
-                        //angular.forEach(userObj, function(userProps, key) {
-
-                        //});
                     });
 
                     return byId;
